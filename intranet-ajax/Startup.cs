2a42350feae7e437d9b5c72a630fe076b4692a0f
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-11-30 09:49:27 -0600

    Add project files.

---
+++ intranet-ajax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(intranet_ajax.Startup))]
+namespace intranet_ajax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
